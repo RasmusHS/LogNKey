ee70aa3563051087d243f9fc5afa6b983ec0f29e
Rasmus Høy Schmidt <rasshoey@gmail.com>
2023-04-19 11:16:12 +0200

    Tilføjet while loop

--- Services/PassCheckerService/PassChecker.py
+++ Services/PassCheckerService/PassChecker.py
@@ -8,23 +8,30 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
+#Chops up the password into individual characters and puts them into an array
 def word(password):
     character=[]
     for i in password:
         character.append(i)
     return character
 
+#Sets the name for the model
 modelName = 'PassCheckModel.pkl'
 
+#Checks if the model already exists
 if os.path.exists('PassCheckModel.pkl'):
+    #Loads the model
     print("Loading model from disk... ")
     with open(modelName, 'rb') as f:
         model, tdif = pickle.load(f)
-        
+
+#If there's no saved model       
 else:
+    #Loads the dataset used to train and test the model
     print("Training model...")
     data = pd.read_csv("C:/Users/Rasmus Høy Schmidt/source/repos/4. Semester/Eksamen/LogNKey/Services/PassCheckerService/Data/data.csv", on_bad_lines='skip')
     
+    #Changes the password strength scores from numbers to strings
     data = data.dropna()
     data["strength"] = data["strength"].map({
         0: "Weak",                          
@@ -32,25 +39,38 @@ else:
         2: "Strong"
         })
     
+    #Puts each column into each their own array 
     x = np.array(data["password"])
     y = np.array(data["strength"])
     print("tokenizing...")
     
+    #Trains the model
     tdif = TfidfVectorizer(tokenizer=word)
     x = tdif.fit_transform(x)
     xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.05, random_state=42)
     print("Training...")
     
+    #Tests the model
     model = RandomForestClassifier()
     print("Testing... - This may take a while")
     model.fit(xtrain, ytrain)
     print("Accuracy", model.score(xtest, ytest))
     
+    #Saves the model
     with open(modelName, "wb") as f:  
         pickle.dump((model, tdif), f)
     
-print("Enter a password to test its strength")
-user = getpass.getpass("Enter Password: ")
-data = tdif.transform([user]).toarray()
-output = model.predict(data)
-print(output)
+Loop = ""
+
+#Prompts the user to enter a password
+while Loop != "stop":
+    print("Enter a password to test its strength")
+    user = getpass.getpass("Enter Password: ")
+    Loop = user
+    data = tdif.transform([user]).toarray()
+    output = model.predict(data)
+    print(output)
+    print("To stop enter 'stop' without quotation")
+    
+else:
+    print("Stopped checking passwords. Re-run the script to start again.")
