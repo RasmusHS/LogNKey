8af7f289a09141ae006bf9e73b2add1e4ddd307b
Rasmus Høy Schmidt <rasshoey@gmail.com>
2023-04-26 17:07:58 +0200

    bb

---
+++ Services/PassGenService/PassGenDomain/PasswordEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassGenDomain
+{
+    //Det er her password genereringsprocessen ligger 
+    public class PasswordEntity
+    {
+
+        public PasswordEntity()
+        {
+
+        }
+
+
+        public string Password { get; private set; }
+        public int Length { get; private set; }
+        public bool MustContainUppercase { get; private set; }
+        public bool MustContainLowercase { get; private set; }
+        public bool MustContainNumbers { get; private set; }
+        public bool MustContainSpecialCharacters { get; private set; }
+
+        public string CreatePassword(int length, bool mustContainUppercase, bool mustContainLowercase, bool mustContainNumbers, bool mustContainSpecialCharacters)
+        {
+            return new NotImplementedException().ToString();
+        }
+    }
+}
